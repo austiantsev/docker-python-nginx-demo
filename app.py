c3389dcfee0f8dae38d4137cc6826af5f57f69b2
Artem Ustiantsev <119280553+austiantsev@users.noreply.github.com>
2026-09-02 14:46:30 +0400

    Create release 2 for rollback test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        response = f"Deployed automatically via CD | {MESSAGE}"
+        response = f"Release 2 | {MESSAGE}"
         self.wfile.write(response.encode())
 
 server = HTTPServer(("0.0.0.0", PORT), Handler)
